d815f13b90ac5320c807d0db274cde8311edc77f
Your Name <marcosralbrecht@gmail.com>
2024-02-06 23:42:26 -0300

    initial configuration chat and socket.io

--- lib/src/pages/chat/controller/chat_service.dart
+++ lib/src/pages/chat/controller/chat_service.dart
@@ -4,40 +4,40 @@ import 'package:get/get.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatService extends GetxService {
+  final url = 'https://sandbox.api.prestadio.com.br/chat';
   final utilServices = UtilServices();
   final hasUnreadMessages = false.obs;
+  late String token;
 
-  late IO.Socket _socket;
+  late IO.Socket socket;
 
   final receiveMessageCallbacks = <Function>[];
 
   @override
-  void onInit() {
+  Future<void> onInit() async {
     super.onInit();
-    initSocket();
+
+    connect();
   }
 
-  void initSocket() async {
-    IO.Socket socket = IO.io('http://localhost:3000');
-    socket.onConnect((_) {
-      print('connect');
-      socket.emit('msg', 'test');
-    });
-    socket.on('event', (data) => print(data));
+  void connect() async {
+    token = await utilServices.getToken();
+
+    socket = IO.io(
+        url,
+        IO.OptionBuilder().setTransports(['websocket']) // for Flutter or Dart VM
+            //.setExtraHeaders({'Authorization': token})
+            .setQuery({'Authorization': token})
+            // optional
+            .build());
+
+    _connectServer();
+  }
+
+  _connectServer() {
+    socket.onConnect((data) => print('Connected on server'));
+    socket.onConnectError((data) => print('Error on Connected server ' + data));
     socket.onDisconnect((_) => print('disconnect'));
-    socket.on('fromServer', (_) => print(_));
-  }
-
-  // Método chamado ao receber uma nova mensagem
-  void onReceiveMessage(dynamic data) {
-    // Notifique todos os callbacks registrados
-    for (var callback in receiveMessageCallbacks) {
-      callback(data);
-    }
-  }
-
-  // Método para registrar um callback para receber mensagens
-  void registrarCallbackReceberMensagem(Function callback) {
-    receiveMessageCallbacks.add(callback);
+    socket.on('receive_message', (data) => print(data));
   }
 }
